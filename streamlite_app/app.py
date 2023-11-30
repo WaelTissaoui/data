d6407ff276575dc29c958414fbe697867d5d4d9b
WaelTissaoui <61127423+WaelTissaoui@users.noreply.github.com>
2023-11-30 11:57:25 +0100

    Add files via upload

---
+++ streamlite_app/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="proboutik_app",page_icon=":japanese_application_button:")
+st.subheader("Welcome to proboutik app :wave:")
+st.title("proboutik dashboard will be here")
